346cd510b51a4e97f3d045471408262aab2daa1e
iwasawa tomomichi <iwasawa-tomomichi@ksc.co.jp>
2019-02-16 14:02:21 +0900

    プロファイル公開開発

--- frontend.API/blc-frontend.API/Controllers/IwaController.cs
+++ frontend.API/blc-frontend.API/Controllers/IwaController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ using System.Web.Http;
 using frontend.API.Models;
 using frontend.API.Models.UI;
 using frontend.API.Services;
+using System.Text;
 
 namespace frontend.API.Controllers
 {
@@ -31,7 +33,7 @@ namespace frontend.API.Controllers
 
         [HttpPost]
         [Route("sendproofs")]
-        public async Task<Hashtable> SendProofs([FromBody] SendProofRequest request)
+        public async Task<dynamic> SendProofs([FromBody] SendProofRequest request)
         {
             // Create address to send profiles
             var destination = await _profileService.CreateProfile(request.TargetOrgAddress);
@@ -40,8 +42,8 @@ namespace frontend.API.Controllers
             var signature = DigitalSignature.FromKey(
                 Convert.FromBase64String(destination.PrivateKey), 
                 Convert.FromBase64String(destination.PublicKey));
-            var value = string.Join(", ", request.ProfileAddressList);
-            byte[] signedValue = signature.Sign(System.Text.Encoding.ASCII.GetBytes(value));
+            var joindAddresses = string.Join(", ", request.ProfileAddressList);
+            byte[] signedValue = signature.Sign(System.Text.Encoding.ASCII.GetBytes(joindAddresses));
 
             // send profile addresses
             var param = new Hashtable();
@@ -51,8 +53,15 @@ namespace frontend.API.Controllers
             param["pubKey"] = destination.PublicKey;
             param["profileAddressList"] = request.ProfileAddressList;
 
-            return param;
-             //await _repository.SendProofs(param);
+            await _repository.SendProofs(param);
+
+            // create access token
+            // TODO: encrypted token by company private key
+            var token = Convert.ToBase64String(Encoding.UTF8.GetBytes(joindAddresses));
+            return new {
+                AccessToken = token,
+                Destination = destination
+            };
         }
     }
 }

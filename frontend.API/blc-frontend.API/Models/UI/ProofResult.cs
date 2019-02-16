cac698f2c8d1b17f10ef535c27cbd67488262a49
37007 <sakuma-takahito@ksc.co.jp>
2019-02-16 19:50:25 +0900

    refactored

---
+++ frontend.API/blc-frontend.API/Models/UI/ProofResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace frontend.API.Models.UI
+{
+    public class ProofResult
+    {
+        public string Id;
+        public bool Verified;
+        public DateTime DateTime;
+        public string Value;
+        public string OrgName;
+    }
+}
---
+++ frontend.API/blc-frontend.API/Utility/EncodingUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace frontend.API.Utility
+{
+    public class EncodingUtil
+    {
+        public static Encoding GetEncoding()
+        {
+            return Encoding.UTF8;
+        }
+    }
+}
